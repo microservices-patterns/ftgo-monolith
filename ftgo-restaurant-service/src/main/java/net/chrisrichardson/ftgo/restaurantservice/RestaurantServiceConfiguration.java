918456fed829f3a5dfb2bcadcbc3471dcbd47ae4
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2018-12-11 17:42:13 +0300

    Removed redundant code.

--- ftgo-restaurant-service/src/main/java/net/chrisrichardson/ftgo/restaurantservice/RestaurantServiceConfiguration.java
+++ ftgo-restaurant-service/src/main/java/net/chrisrichardson/ftgo/restaurantservice/RestaurantServiceConfiguration.java
@@ -2,18 +2,14 @@ package net.chrisrichardson.ftgo.restaurantservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.eventuate.javaclient.commonimpl.JSonMapper;
-import io.eventuate.jdbckafka.TramJdbcKafkaConfiguration;
-import io.eventuate.tram.commands.common.ChannelMapping;
-import io.eventuate.tram.commands.common.DefaultChannelMapping;
 import net.chrisrichardson.eventstore.examples.customersandorders.commonswagger.CommonSwaggerConfiguration;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.*;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-@Import({TramJdbcKafkaConfiguration.class, CommonSwaggerConfiguration.class})
+@Import({CommonSwaggerConfiguration.class})
 public class RestaurantServiceConfiguration {
   @Bean
   @Primary // conflicts with _halObjectMapper
