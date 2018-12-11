52446b3cc6dbdbfdb7277ac62c7eec6fe97092b0
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2018-12-11 18:37:29 +0300

    Removed redundant code.

--- ftgo-restaurant-service-aws-lambda/src/main/java/net/chrisrichardson/ftgo/restaurantservice/lambda/RestaurantServiceLambdaConfiguration.java
+++ ftgo-restaurant-service-aws-lambda/src/main/java/net/chrisrichardson/ftgo/restaurantservice/lambda/RestaurantServiceLambdaConfiguration.java
@@ -1,13 +1,12 @@
 package net.chrisrichardson.ftgo.restaurantservice.lambda;
 
-import io.eventuate.tram.messaging.producer.jdbc.TramMessageProducerJdbcConfiguration;
 import net.chrisrichardson.ftgo.restaurantservice.domain.RestaurantServiceDomainConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({RestaurantServiceDomainConfiguration.class, TramMessageProducerJdbcConfiguration.class})
+@Import(RestaurantServiceDomainConfiguration.class)
 @EnableAutoConfiguration
 public class RestaurantServiceLambdaConfiguration {
 }
