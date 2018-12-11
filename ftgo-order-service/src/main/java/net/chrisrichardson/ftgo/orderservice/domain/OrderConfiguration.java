acbaa50f14602e44f0616f11ad781f27ada00d0a
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2018-12-11 16:49:33 +0300

    Removed redundant code.

--- ftgo-order-service/src/main/java/net/chrisrichardson/ftgo/orderservice/domain/OrderConfiguration.java
+++ ftgo-order-service/src/main/java/net/chrisrichardson/ftgo/orderservice/domain/OrderConfiguration.java
@@ -1,11 +1,7 @@
 package net.chrisrichardson.ftgo.orderservice.domain;
 
-import io.eventuate.tram.events.publisher.TramEventsPublisherConfiguration;
-import io.eventuate.tram.sagas.orchestration.SagaCommandProducer;
-import io.eventuate.tram.sagas.orchestration.SagaOrchestratorConfiguration;
 import io.micrometer.core.instrument.MeterRegistry;
 import net.chrisrichardson.ftgo.accountingservice.domain.AccountingService;
-import net.chrisrichardson.ftgo.common.CommonConfiguration;
 import net.chrisrichardson.ftgo.common.RestaurantRepository;
 import net.chrisrichardson.ftgo.consumerservice.domain.ConsumerService;
 import net.chrisrichardson.ftgo.kitchenservice.domain.KitchenService;
@@ -16,7 +12,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 import java.util.Optional;
 
@@ -24,15 +19,8 @@ import java.util.Optional;
 @EnableAutoConfiguration
 @ComponentScan
 @EntityScan
-@Import({CommonConfiguration.class, TramEventsPublisherConfiguration.class, SagaOrchestratorConfiguration.class})
 public class OrderConfiguration {
   // TODO move to framework
-
-  @Bean
-  public SagaCommandProducer sagaCommandProducer() {
-    return new SagaCommandProducer();
-  }
-
   @Bean
   public OrderService orderService(RestaurantRepository restaurantRepository,
                                    OrderRepository orderRepository,
